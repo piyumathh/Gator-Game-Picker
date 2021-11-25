46f9b2dcc448d4df8772e92f502ac1d7beb044ce
tamhuynh3861 <90296519+tamhuynh3861@users.noreply.github.com>
2021-11-25 02:31:40 -0500

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,32 @@
+//
+// Created by tamhu on 11/25/2021.
+//
+
+#include <string>
+
+class Game {
+private:
+    string name;
+    string releaseDate;
+    string website;
+    string esrb_Rating;
+    string genre; // possibly a vector
+    string platform; // possibly a vector
+    string publisher;
+    string developer;
+    int update;
+    int numAchievements;
+    int playTime;
+    int numSuggestions;
+    double rating;
+
+public:
+    void printInfo();
+    Game();
+    Game(string[] arr, int[] arr2, double rating); // put info in array to clean it up
+};
+
+
+
+
+#endif //PROJECT3FILES_GAME_H
